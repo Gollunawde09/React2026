7bc780a3c7f9c96f0300520a129fc46ddd102bad
Gollunawde09 <nawdego@gmail.com>
2026-02-07 19:55:47 +0530

    Two Way Binding Form Elements

---
+++ CRUD2/src/App.jsx
@@ -0,0 +1,57 @@
+import { useState } from "react"
+
+const App = ( ) => {
+  const [todos, settodos] = useState ([
+    { id :1, title: "Kamm krle bhai", isComplated: false },
+  ]);
+
+  const [title, settitle] = useState("");
+  const [completed, setcompleted] = useState(false);
+  const [gender, setgender] = useState("male");
+  const [city, setcity] = useState("Delhi")
+
+  return (
+    <div>
+      <h1>Create Tasks</h1>
+      <form >
+        <input
+        onChange={(e) => settitle(e.target.value)}
+        type="text"
+        placeholder="title" />
+        <br />
+        <br />
+        <input  
+        checked={completed} // props react // html atribute 
+        onChange={(e) => setcompleted(e.target.checked)} // props react // html atribute 
+        type="checkbox"/> Completed 
+        <br />
+        <br />
+        <input  
+         onChange={(e) => 
+          console.log(e.target.value)}
+          checked={gender == "male" && true}
+          type="radio"  
+          value="male"
+        /> male 
+        <input  
+         onChange={(e) => 
+          console.log(e.target.value)}
+          checked={gender == "female" && true}
+          type="radio"  
+          value="female"
+        /> female
+        <br />
+        <br />
+        City :
+        <select onChange={(e) => setcity(e.target.value)}>
+       <option value="Delhi">Delhi</option>
+       <option value="Mumbai">Mumbai</option>
+       <option value="kolkata">Kolkata</option>          
+        </select><br />
+        <button> Create Todo</button>
+      </form>
+    </div>
+  );
+};
+
+export default App;
